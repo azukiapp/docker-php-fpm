a5d4480fd1ab2d4b5efc94e251489bd2edc0de5a
Gullit Miranda <gullitmiranda@gmail.com>
2015-04-30 16:51:26 -0300

    Adding dynamic support projects with the `public` folder for nginx, which are commonly used for frameworks.

--- 5.6/run.sh
+++ 5.6/run.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-if [[ "$APP_DIR" ]]; then
-  rm -rf /app
-  ln -s $APP_DIR /app
+
+if [ ! -z "$APP_DIR" ]; then
+	DOCUMENT_ROOT="/app"
+	rm -rf ${DOCUMENT_ROOT}
+
+	if [ -d "${APP_DIR}/public" ]; then
+		dir="${DOCUMENT_ROOT}"
+	else
+		dir="${DOCUMENT_ROOT}/public"
+	fi
+
+	ln -s $APP_DIR $dir
 fi
+
 service php5-fpm start && nginx
