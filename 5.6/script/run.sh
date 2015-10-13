10136a87e53e77fb3f5e8bdf8c26f9f88fc26922
Gullit Miranda <gullitmiranda@gmail.com>
2015-10-13 17:19:17 -0300

    Fixing build and CI

--- 5.6/script/run.sh
+++ 5.6/script/run.sh
@@ -1,29 +1,24 @@
 DOCUMENT_ROOT="/var/www"
 
 if [ ! -z "$PUBLIC_DIR" ]; then
-    rm -rf "${DOCUMENT_ROOT}"
-    mkdir -p "${DOCUMENT_ROOT}"
+  rm -rf "${DOCUMENT_ROOT}"
+  mkdir -p "${DOCUMENT_ROOT}"
 
-    APP_DIR="${PUBLIC_DIR}"
-    dir="${DOCUMENT_ROOT}/public"
+  APP_DIR="${PUBLIC_DIR}"
+  dir="${DOCUMENT_ROOT}/public"
 elif [ ! -z "$APP_DIR" ]; then
-    rm -rf "${DOCUMENT_ROOT}"
+  rm -rf "${DOCUMENT_ROOT}"
 
-    if [ -d "${APP_DIR}/public" ]; then
-        dir="${DOCUMENT_ROOT}"
-    else
-        dir="${DOCUMENT_ROOT}/public"
-    fi
+  if [ -d "${APP_DIR}/public" ]; then
+      dir="${DOCUMENT_ROOT}"
+  else
+      dir="${DOCUMENT_ROOT}/public"
+  fi
 
-    mkdir -p "dirname ${dir}"
+  mkdir -p "dirname ${dir}"
 fi
 
-echo "linking public folder: ${APP_DIR} -> ${dir}"
-ln -s $APP_DIR $dir
-
-# # Tweak nginx to match the workers to cpu's
-# procs=$(cat /proc/cpuinfo |grep processor | wc -l)
-# sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
-
-# php5-fpm --allow-to-run-as-root --nodaemonize --fpm-config /etc/php5/fpm/php-fpm.conf &
-# exec nginx -g daemon off
+if [ ! -z "$APP_DIR" ]; then
+  echo "linking public folder: ${APP_DIR} -> ${dir}"
+  ln -s $APP_DIR $dir
+fi
